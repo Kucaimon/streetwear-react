9f22cfabc118be8920586123bb4ee266a5cbed39
User <user@example.com>
2025-12-01 11:47:50 +0500

    Add GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Для правильной работы на любом хостинге
+  base: '/streetwear-react/',
 })
